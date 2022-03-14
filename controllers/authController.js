bc5108c680537285e8fc3e9c59a7bef503bf82ff
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2022-03-14 18:53:35 +0100

    update authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,7 +66,8 @@ exports.login = catchAsync(async (req,res,next)=>{
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN*24*60*60*1000),
         httpOnly: true,
         secure : true,
-        domain: 'http://localhost:3000'
+        domain: 'http://localhost:3000',
+        sameSite: 'None'
     };
     //if(req.secure || req.headers('x-forwarded-proto')=== 'https') cookieOption.secure = true;
 
@@ -106,7 +107,8 @@ exports.googleLogin = catchAsync(async (req, res, next)=> {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN*24*60*60*1000),
         httpOnly: true,
         secure : true,
-        domain: 'http://localhost:3000'
+        domain: 'http://localhost:3000',
+        sameSite: 'None'
     };
     //if(req.secure || req.headers('x-forwarded-proto')=== 'https') cookieOption.secure = true;
 
@@ -163,7 +165,8 @@ exports.logout = catchAsync(async (req,res,next)=>{
         expires: new Date(Date.now() + 10*1000),
         httpOnly: true,
         secure : true,
-        domain: 'http://localhost:3000'
+        domain: 'http://localhost:3000',
+        sameSite: 'None'
     };
     //if(req.secure || req.headers('x-forwarded-proto')=== 'https') cookieOption.secure = true;
     res.cookie('jwt', 'logout', cookieOption);
