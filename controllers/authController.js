2a9286b697286dc7c26514103313dfebe0b31cc8
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2022-07-23 15:41:36 +0200

    change authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,7 +62,8 @@ exports.login = catchAsync(async (req,res,next)=>{
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN*24*60*60*1000),
         httpOnly: true,
         secure : true,
-        sameSite: 'none'
+        sameSite: 'none',
+        domain: '.heroku.com'
     };
    // if(req.secure || req.headers('x-forwarded-proto')=== 'https') cookieOption.secure = true;
 
