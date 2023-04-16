bab867ee050ba946ae38445c00df550274ddb971
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2023-04-16 22:49:22 +0200

    edit cors

--- controllers/authController.js
+++ controllers/authController.js
@@ -177,9 +177,9 @@ exports.signup = catchAsync(async (req,res,next)=>{
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN*24*60*60*1000),
         httpOnly: true,
         //secure : true,
-        SameSite: 'None'
+        // SameSite: 'None'
     };
-    if(req.secure || req.headers('x-forwarded-proto')=== 'https') cookieOption.secure = true;
+    // if(req.secure || req.headers('x-forwarded-proto')=== 'https') cookieOption.secure = true;
 
     res.cookie('jwt', token, cookieOption);
     res.status(201).json({
