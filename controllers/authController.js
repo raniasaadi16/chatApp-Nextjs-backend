46a81e695525a38a22d9e01e590c880a8e6f316f
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2023-04-17 01:16:41 +0200

    debug

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,7 +64,7 @@ exports.login = catchAsync(async (req,res,next)=>{
     const token = jwt.sign({id: user.id}, process.env.JWT_SECRET);
     const cookieOption = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN*24*60*60*1000),
-        httpOnly: true,
+        httpOnly: false,
         // secure : true,
         // sameSite: 'none',
         // domain: '.herokuapp.com'
