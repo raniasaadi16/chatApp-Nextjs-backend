40dfbc2abd6b47d1d7f0d2afa1913d7a58e60418
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2023-04-17 22:43:21 +0200

    edit cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,9 +15,11 @@ exports.protect = catchAsync(async (req,res,next)=>{
     let token;
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];   
-    }else if(req.headers.cookie) {
+    }
+    if(req.headers.cookie) {
         token = req.headers.cookie
-    }else if(req.cookies.jwt) {
+    }
+    if(req.cookies.jwt) {
         token = req.cookies.jwt
     };
 
